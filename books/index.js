20d070ce45729dade103972031718038f44535f1
Erick Buitrago <erickbuitrago2014@gmail.com>
2022-03-23 17:30:59 -0500

    fix errors

--- books/index.js
+++ books/index.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use('/book', require('./bookRoute'))
 
 app.listen(4000, () => {
-    console.log('App runnig on port 4000');
+    console.log('App runnig on port 4000 - Books Service');
 });
